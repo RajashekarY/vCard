c63659e03eeaf64b3cff3591512730dbe738c78d
rajashekary <yadavallirajashekar@gmail.com>
2021-06-04 14:47:58 +0530

    Use of Cards insted of columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,25 +15,25 @@ class MyApp extends StatelessWidget {
           child: SafeArea(
             child: Column(
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: 100,
                   backgroundImage: AssetImage('images/developer.jpg'),
                 ),
                 Card(
                   color: Colors.blueGrey[900],
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 10.0),
                   child: Row(
                     // ignore: prefer_const_literals_to_create_immutables
                     children: <Widget>[
-                      Icon(
+                      const Icon(
                         Icons.phone,
                         color: Colors.white,
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 10,
                       ),
-                      Text(
+                      const Text(
                         '+91 8686 5636 75',
                         style: TextStyle(
                             color: Colors.white,
@@ -45,19 +46,19 @@ class MyApp extends StatelessWidget {
                 ),
                 Card(
                   color: Colors.blueGrey[900],
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 10.0),
                   child: Row(
                     // ignore: prefer_const_literals_to_create_immutables
                     children: <Widget>[
-                      Icon(
+                      const Icon(
                         Icons.email,
                         color: Colors.white,
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 10,
                       ),
-                      Text(
+                      const Text(
                         'yadavallirajashekar@gmail.com',
                         style: TextStyle(
                             color: Colors.white,
@@ -76,105 +77,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.teal[300],
-//         body: SafeArea(
-//           child: Center(
-//             child: Column(children: <Widget>[
-//               CircleAvatar(
-//                 radius: 100,
-//                 backgroundImage: AssetImage('images/developer.jpg'),
-//               ),
-//               Text(
-//                 "Raajasekhar",
-//                 textScaleFactor: 2,
-//                 style: TextStyle(
-//                   fontFamily: 'Pacifico',
-//                   letterSpacing: 1,
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               Text(
-//                 'AI Developer',
-//                 style: TextStyle(
-//                   fontFamily: 'SourceSansPro',
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.bold,
-//                   letterSpacing: 2.5,
-//                 ),
-//               ),
-//               Container(
-//                 color: Colors.white,
-//                 padding: EdgeInsets.symmetric(),
-//                 child: Center(
-//                   child: Row(
-//                     // mainAxisAlignment: ,
-//                     children: <Widget>[
-//                       const Icon(
-//                         Icons.phone,
-//                         size: 25,
-//                         color: Colors.teal,
-//                       ),
-//                       const SizedBox(
-//                         width: 10,
-//                       ),
-//                       const Text(
-//                         '+91 8686 5636 75',
-//                         style: TextStyle(
-//                           fontFamily: 'SourceSansPro',
-//                           color: Colors.teal,
-//                           fontSize: 20,
-//                           letterSpacing: 2.5,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(
-//                 width: 20,
-//                 height: 25,
-//               ),
-//               // SizedBox()
-//               Container(
-//                 color: Colors.white,
-//                 padding: EdgeInsets.symmetric(),
-//                 child: Center(
-//                   child: Row(
-//                     // mainAxisAlignment: ,
-//                     children: <Widget>[
-//                       Icon(
-//                         Icons.mail,
-//                         size: 20,
-//                         color: Colors.teal[500],
-//                       ),
-//                       SizedBox(
-//                         width: 10,
-//                       ),
-//                       Text(
-//                         'yadavallirajashekar@gmail.com',
-//                         style: TextStyle(
-//                             fontSize: 20,
-//                             // color: Colors.white,
-//                             color: Colors.teal[500],
-//                             fontFamily: 'SourceSansPro'),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ]),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
