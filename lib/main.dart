970016263f9b53e8e3356f51d0b29ba6d2f8b3e5
rajashekary <yadavallirajashekar@gmail.com>
2021-06-05 11:49:11 +0530

    Final Stateless vCard

--- lib/main.dart
+++ lib/main.dart
@@ -11,66 +11,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: SafeArea(
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('images/developer.jpg'),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/developer.jpg'),
+              ),
+              Text(
+                'Raajasekhar',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                Card(
-                  color: Colors.blueGrey[900],
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 10.0),
-                  child: Row(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: <Widget>[
-                      const Icon(
-                        Icons.phone,
-                        color: Colors.white,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      const Text(
-                        '+91 8686 5636 75',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'SourceSansPro',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
+              ),
+              Text(
+                'AI Developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.white,
+                  fontSize: 14.0,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 300.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.blueGrey[900],
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: const ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    '+91 8686 563 XXX',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
                   ),
                 ),
-                Card(
-                  color: Colors.blueGrey[900],
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 10.0),
-                  child: Row(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: <Widget>[
-                      const Icon(
-                        Icons.email,
-                        color: Colors.white,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      const Text(
-                        'yadavallirajashekar@gmail.com',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'SourceSansPro',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
+              ),
+              SizedBox(width: 10.0),
+              Card(
+                color: Colors.blueGrey[900],
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(Icons.email, color: Colors.white),
+                  title: Text(
+                    'XXXXrajashekar@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'SourceSansPro',
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
